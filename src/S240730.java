5d5f5d6f20d34113f8913c78d1eb46594c17361c
sinh8 <aktms252@naver.com>
2024-07-30 22:47:47 +0900

    240730 코드업 1023

--- src/S240730.java
+++ src/S240730.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class S240730 {
     public static void main(String[] args)
     {
-        System.out.println("good");
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+
+        String b = Double.toString(a);
+
+        String[] parts = b.split("\\.");
+
+        System.out.printf("%s\n%s", parts[0], parts[1]);
     }
 }
